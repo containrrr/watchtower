ff277f99036faa0c0a66deb57a204b3ec7d827b6
Jamie Hannaford <jamie.hannaford@rackspace.com>
2016-03-15 17:12:58 +0100

    Add godeps

--- Godeps/_workspace/src/github.com/samalba/dockerclient/auth.go
+++ Godeps/_workspace/src/github.com/samalba/dockerclient/auth.go
@@ -8,14 +8,32 @@ import (
 
 // AuthConfig hold parameters for authenticating with the docker registry
 type AuthConfig struct {
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
-	Email    string `json:"email,omitempty"`
+	Username      string `json:"username,omitempty"`
+	Password      string `json:"password,omitempty"`
+	Email         string `json:"email,omitempty"`
+	RegistryToken string `json:"registrytoken,omitempty"`
 }
 
 // encode the auth configuration struct into base64 for the X-Registry-Auth header
-func (c *AuthConfig) encode() string {
+func (c *AuthConfig) encode() (string, error) {
 	var buf bytes.Buffer
-	json.NewEncoder(&buf).Encode(c)
-	return base64.URLEncoding.EncodeToString(buf.Bytes())
+	if err := json.NewEncoder(&buf).Encode(c); err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(buf.Bytes()), nil
+}
+
+// ConfigFile holds parameters for authenticating during a BuildImage request
+type ConfigFile struct {
+	Configs  map[string]AuthConfig `json:"configs,omitempty"`
+	rootPath string
+}
+
+// encode the configuration struct into base64 for the X-Registry-Config header
+func (c *ConfigFile) encode() (string, error) {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(c); err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(buf.Bytes()), nil
 }
