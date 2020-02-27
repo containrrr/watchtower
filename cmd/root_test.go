25907109fd0063b6e9261967290d88ae951d22ff
Seydou Fofana <seydou.fofana@it-economics.de>
2020-02-27 15:27:49 +0100

    clean up and tidying

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -4,66 +4,11 @@ import (
 	"testing"
 
 	"github.com/spf13/cobra"
-<<<<<<< HEAD
-	"github.com/spf13/viper"
-=======
->>>>>>> 9da817df68673fd4ef0d58b7219d99fb1d79d55e
 	"github.com/stretchr/testify/assert"
 
 	"github.com/containrrr/watchtower/internal/flags"
 )
 
-<<<<<<< HEAD
-func TestComputeMaxMemoryPerContainerInByte_default_2G_applyFlagSet(t *testing.T) {
-	cmd := new(cobra.Command)
-	defaultMaxMemory := int64(2 * (1024 * 1024 * 1024))
-
-	viper.Set("APPLY_RESOURCE_LIMIT", true)
-	initializeCmd(cmd)
-	PreRun(cmd, cmd.Flags().Args())
-
-	assert.Equal(t, defaultMaxMemory, maxMemoryPerContainer)
-}
-
-func TestComputeMaxMemoryPerContainerInByte_applyFlagNotSet(t *testing.T) {
-	cmd := new(cobra.Command)
-
-	initializeCmd(cmd)
-	PreRun(cmd, cmd.Flags().Args())
-
-	//	assert.Equal(t, int64(0), maxMemoryPerContainer)
-}
-
-/*
-func TestComputeMaxMemoryPerContainerInByte_custom_4G(t *testing.T) {
-	cmd := new(cobra.Command)
-	defaultMaxMemory := int64(4 * (1024 * 1024 * 1024))
-
-	//args := []string{"max-memory-per-container", "4G"}
-	cmd.Flags().StringP("--max-memory-per-container", "", "4G", "max-memory-per-container")
-	initializeCmd(cmd)
-	//rootCmd.SetArgs(args)
-	//Execute()
-	//Run(rootCmd, args)
-	PreRun(cmd, cmd.Flags().Args())
-
-	assert.Equal(t, defaultMaxMemory, maxMemoryPerContainer)
-}
-
-/*
-func TestComputeMaxMemoryPerContainerInByte_custom_512m(t *testing.T) {
-	cmd := new(cobra.Command)
-	defaultMaxMemory := int64(512 * (1024 * 1024))
-
-	cmd.SetArgs(strings.Split("max-memory-per-container 512M", " "))
-	initializeCmd(cmd)
-	//args := []string{"max-memory-per-container", "512M"}
-	PreRun(cmd, cmd.Flags().Args())
-	//Execute()
-	assert.Equal(t, defaultMaxMemory, maxMemoryPerContainer)
-}
-*/
-=======
 func TestComputeMaxMemoryPerContainerInByte_shouldBe0(t *testing.T) {
 	cmd := new(cobra.Command)
 	initializeCmd(cmd)
@@ -72,7 +17,6 @@ func TestComputeMaxMemoryPerContainerInByte_shouldBe0(t *testing.T) {
 	assert.Equal(t, int64(0), maxMemoryPerContainer)
 }
 
->>>>>>> 9da817df68673fd4ef0d58b7219d99fb1d79d55e
 func initializeCmd(cmd *cobra.Command) {
 	flags.SetDefaults()
 	flags.RegisterDockerFlags(cmd)
