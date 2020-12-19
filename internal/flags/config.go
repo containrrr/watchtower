4e69282890ce1407d5269896c1b0ed4e363ecb0d
nils måsén <nils@piksel.se>
2020-12-19 12:48:00 +0100

    fix linting errors

--- internal/flags/config.go
+++ internal/flags/config.go
@@ -1,7 +1,7 @@
 package flags
 
 import (
-	. "time"
+	"time"
 )
 
 // WatchConfig is the global watchtower configuration created from flags and environment variables
@@ -16,16 +16,16 @@ type WatchConfig struct {
 	EnableLabel       bool `mapstructure:"label-enable"`
 	Debug             bool
 	Trace             bool
-	MonitorOnly       bool     `mapstructure:"monitor-only"`
-	RunOnce           bool     `mapstructure:"run-once"`
-	IncludeStopped    bool     `mapstructure:"include-stopped"`
-	IncludeRestarting bool     `mapstructure:"include-restarting"`
-	ReviveStopped     bool     `mapstructure:"revive-stopped"`
-	LifecycleHooks    bool     `mapstructure:"enable-lifecycle-hooks"`
-	RollingRestart    bool     `mapstructure:"rolling-restart"`
-	HTTPAPI           bool     `mapstructure:"http-api"`
-	HTTPAPIToken      string   `mapstructure:"http-api-token"`
-	Timeout           Duration `mapstructure:"stop-timeout"`
+	MonitorOnly       bool          `mapstructure:"monitor-only"`
+	RunOnce           bool          `mapstructure:"run-once"`
+	IncludeStopped    bool          `mapstructure:"include-stopped"`
+	IncludeRestarting bool          `mapstructure:"include-restarting"`
+	ReviveStopped     bool          `mapstructure:"revive-stopped"`
+	LifecycleHooks    bool          `mapstructure:"enable-lifecycle-hooks"`
+	RollingRestart    bool          `mapstructure:"rolling-restart"`
+	HTTPAPI           bool          `mapstructure:"http-api"`
+	HTTPAPIToken      string        `mapstructure:"http-api-token"`
+	Timeout           time.Duration `mapstructure:"stop-timeout"`
 	Scope             string
 	NoColor           bool `mapstructure:"no-color"`
 }
