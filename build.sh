5f867b56c0d28e691dcab326d4cc10fe622cfd30
nils måsén <nils@piksel.se>
2021-06-11 16:23:04 +0200

    fix: move build meta to own package
    this allows it to be referenced from other packages without causing a cyclic dependency

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 VERSION=$(git describe --tags)
 echo "Building $VERSION..."
-go build -o watchtower -ldflags "-X github.com/containrrr/watchtower/cmd.version=$VERSION"
+go build -o watchtower -ldflags "-X github.com/containrrr/watchtower/internal/meta.Version=$VERSION"
