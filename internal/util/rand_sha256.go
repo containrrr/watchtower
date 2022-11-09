637114aeee97411e7a88b50849e7dde4e2b4238c
nils måsén <nils@piksel.se>
2022-11-09 12:32:41 +0100

    feat(clean): log removed/untagged images

---
+++ internal/util/rand_sha256.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+)
+
+// GenerateRandomSHA256 generates a random 64 character SHA 256 hash string
+func GenerateRandomSHA256() string {
+	return GenerateRandomPrefixedSHA256()[7:]
+}
+
+// GenerateRandomPrefixedSHA256 generates a random 64 character SHA 256 hash string, prefixed with `sha256:`
+func GenerateRandomPrefixedSHA256() string {
+	hash := make([]byte, 32)
+	_, _ = rand.Read(hash)
+	sb := bytes.NewBufferString("sha256:")
+	sb.Grow(64)
+	for _, h := range hash {
+		_, _ = fmt.Fprintf(sb, "%02x", h)
+	}
+	return sb.String()
+}
